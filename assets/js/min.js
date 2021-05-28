513118b940ef443e1d37ae56d898060798113327
sujon mia <sujonbdjoin019@gmail.com>
2021-05-29 00:35:28 +0600

    header top design

--- assets/js/min.js
+++ assets/js/min.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
+    // Header Scrolling 
     let header = 0;
     $(window).on('scroll', function(){
         $('#headers').toggleClass('hide', $(window).scrollTop() > header);
         header = $(window).scrollTop(); 
     });
+
+    // tooltrip 
+   
 
 });
